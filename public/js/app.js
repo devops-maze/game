fc27127161df1100de401dbf955aa5bc5edf8439
Jankelic Ivan <ivan.jankela20@gmail.com>
2020-11-04 19:59:59 +0100

    difficulty highscore dropdown

--- public/js/app.js
+++ public/js/app.js
@@ -67,7 +67,7 @@ function getUserScore() {
 /* opens the profile button */
 
 function profileOnClick() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("profile-dropdown").classList.toggle("show");
 }
 
 //closes the profile button
